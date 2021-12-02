5f75af27642f94aa36fe3ee595020af3e602d80a
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-01 21:04:42 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -69,13 +69,13 @@ def main(sc):
   datedf = weeklydf.select('safegraph_place_id',
                  F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'))\
                 .filter(F.col("date") >= datetime.date(2019,1,1))
-  datedf.write.option("header",True).csv(f"/{sys.argv[1]}/{fileNames[index]}")
+  #datedf.write.option("header",True).csv(f"/{sys.argv[1]}/{fileNames[index]}")
   NAICS = set(['452210', '452311', '445120', '722410', '722511', '722513', '446110', '446191','311811', '722515', 
              '445210','445220','445230','445291','445292','445299','445110'])
   coredf = spark.read.csv('hdfs:///data/share/bdm/core-places-nyc.csv', header = True, escape = '"')\
           .select('safegraph_place_id','naics_code')\
           .where(F.col('naics_code').isin(NAICS)) 
-  index+=1
+  #index+=1
   coredf.write.option("header",True).csv(f"{sys.argv[1]}/{fileNames[index]}")
 #.rdd.saveAsTextFile(f"{sys.argv[1]}/{fileNames[index]}")
   #joindf = coredf.join(datedf, 'safegraph_place_id')\
