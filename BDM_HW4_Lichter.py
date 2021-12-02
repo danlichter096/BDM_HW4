a26588381f3bc2aab027f0eb60b96b028a91919d
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-01 22:20:46 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -94,7 +94,8 @@ def main(sc):
   #joindf.write.option("header",True).csv(f"{sys.argv[1]}/{fileNames[index]}")
   #for x in range(len(dfs)):
   #index+=5
-  a_df = big_box_df.groupBy('year','date').agg(F.stddev_pop('visits').alias('std')) #, F.sort_array(F.collect_list('visits')).alias('array1'))#\
+  a_df = big_box_df.groupBy('year','date').agg(F.sort_array(F.collect_list('visits')).alias('array1'))#\
+#  F.stddev_pop('visits').alias('std')) #,
   a_df.write.option("header",True).csv(f"{sys.argv[1]}/{fileNames[index]}")
   #      .withColumn('median', F.element_at(F.col('array1'), F.ceil((F.size(F.col('array1'))/2)).cast('int')))\
   #      .withColumn('std', F.round('std').cast('int'))\
