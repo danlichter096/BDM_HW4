9b5642e797a3a3744b4f3e9e77ad0164eb9339a8
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-01 21:14:25 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -74,8 +74,8 @@ def main(sc):
   coredf = spark.read.csv('hdfs:///data/share/bdm/core-places-nyc.csv', header = True, escape = '"')\
           .select('safegraph_place_id','naics_code')\
           .where(F.col('naics_code').isin(NAICS)) 
-  joindf = coredf.join(datedf, 'safegraph_place_id')#\
-#              .withColumn('year', F.year(F.col('date')))\
+  joindf = coredf.join(datedf, 'safegraph_place_id')\
+               .withColumn('year', F.year(F.col('date')))#\
 #              .withColumn('date', F.expr("make_date(2020,month(date),dayofmonth(date))"))\
 #              .select('naics_code', 'year','date', 'visits')
   joindf.write.option("header",True).csv(f"{sys.argv[1]}/{fileNames[index]}")
