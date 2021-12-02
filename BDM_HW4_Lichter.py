3103b9dc9003992d4da4c6795df5c07f8d31844d
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-01 22:38:28 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -23,42 +23,8 @@ def expandVisits(date_range_start, visits_by_day):
         datetime_date+=datetime.timedelta(days=1)
     return visits
 
-def importData(spark):
-  weeklydf = spark.read.csv('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/', header = True)\
-                .select('safegraph_place_id', 'date_range_start', 'visits_by_day')
-  udfExpand = F.udf(expandVisits, MapType(DateType(), IntegerType()))
-  datedf = weeklydf.select('safegraph_place_id',
-                 F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'))\
-                .filter(F.col("date") >= datetime.date(2019,1,1))
-  NAICS = set(['452210', '452311', '445120', '722410', '722511', '722513', '446110', '446191','311811', '722515', 
-             '445210','445220','445230','445291','445292','445299','445110'])
-  coredf = spark.read.csv('hdfs:///data/share/bdm/core-places-nyc.csv', header = True, escape = '"')\
-          .select('safegraph_place_id','naics_code')\
-          .where(F.col('naics_code').isin(NAICS)) 
-  joindf = coredf.join(datedf, 'safegraph_place_id')\
-              .withColumn('year', F.year(F.col('date')))\
-              .withColumn('date', F.expr("make_date(2020,month(date),dayofmonth(date))"))\
-              .select('naics_code', 'year','date', 'visits')
-  return joindf
-
-  def splitData(joindf):
-    big_box_df = joindf.where(F.col('naics_code').isin([452210,452311])).drop('naics_code')
-    convenience_df = joindf.where(F.col('naics_code').isin([445120])).drop('naics_code')
-    drinking_df = joindf.where(F.col('naics_code').isin([722410])).drop('naics_code')
-    full_service_df = joindf.where(F.col('naics_code').isin([722511])).drop('naics_code')
-    limited_service_df = joindf.where(F.col('naics_code').isin([722513])).drop('naics_code')
-    pharmacies_drug_df = joindf.where(F.col('naics_code').isin([446110,446191])).drop('naics_code')
-    snack_bakeries_df = joindf.where(F.col('naics_code').isin([311811,722515])).drop('naics_code')
-    specialty_df = joindf.where(F.col('naics_code').isin([445210,445220,445230,445291,445292,445299])).drop('naics_code')
-    supermarkets_df = joindf.where(F.col('naics_code').isin([445110])).drop('naics_code')
-    list_of_df = [big_box_df, convenience_df, drinking_df, full_service_df, limited_service_df, 
-                pharmacies_drug_df, snack_bakeries_df, specialty_df, supermarkets_df]
-    return list_of_df
-
 def main(sc):
   spark = SparkSession(sc)
-  #joindf = importData(spark)
-  #dfs = splitData(joindf)
   index = 0
   fileNames = ['big_box_grocers','convenience_stores','drinking_places','full_service_restaurants','limited_service_restaurants',
                 'pharmacies_and_drug_stores','snack_and_bakeries','specialty_food_stores','supermarkets_except_convenience_stores']
@@ -91,7 +57,6 @@ def main(sc):
   
   #dfs = [big_box_df, convenience_df, drinking_df, full_service_df, limited_service_df, 
   #              pharmacies_drug_df, snack_bakeries_df, specialty_df, supermarkets_df]
-  #joindf.write.option("header",True).csv(f"{sys.argv[1]}/{fileNames[index]}")
   #for x in range(len(dfs)):
   #index+=5
   a_df = big_box_df.groupBy('year','date').agg(F.stddev_pop('visits').alias('std'), F.sort_array(F.collect_list('visits')).alias('array1'))\
