23d9a8251d1194fafdeb3194b00d1fea381cd477
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-05 15:20:01 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -57,11 +57,12 @@ def main(sc):
   supermarkets_df = joindf.where(F.col('naics_code').isin([445110])).drop('naics_code')
   
   a = big_box_df.groupby('year','date').agg(F.sort_array(F.collect_list('visits')).alias('array_visits'))\
-                      .withColumn('middle', F.ceil(F.size(F.col('array_visits'))/2).cast('int') ).drop('array_visits') #F.ceil((F.size(F.col('array_visits'))/2)).cast('int'))).drop('array_visits')
+                      .withColumn('middle', F.ceil(F.size(F.col('array_visits'))/2).cast('int') )\
+                      .withColumn('median', F.element_at(F.col('array_visits'), 1)).drop('array_visits') #F.ceil((F.size(F.col('array_visits'))/2)).cast('int'))).drop('array_visits')
                       #.withColumn('median', F.element_at(F.col('array_visits'), F.col('middle')))\
                        # .drop('array_visits') #F.element_at(F.col('array_visits'), F.ceil((F.size(F.col('array_visits'))/2)).cast('int')))
   a.write.option("header",True).csv(f"{sys.argv[1]}/a")
-  big_box_df.write.option("header", True).csv(f"{sys.argv[1]}/{a.dtypes}")
+  #big_box_df.write.option("header", True).csv(f"{sys.argv[1]}/{a.dtypes}")
   #convenience_df.write.option("header",True).csv(f"{sys.argv[1]}/convenience_stores")
   #drinking_df.write.option("header",True).csv(f"{sys.argv[1]}/drinking_places")
   #full_service_df.write.option("header",True).csv(f"{sys.argv[1]}/full_service_restaurants")
