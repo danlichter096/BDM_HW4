f84fc403d415ace5dc112f109dac02118a67b150
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-05 13:37:54 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -57,7 +57,7 @@ def main(sc):
   supermarkets_df = joindf.where(F.col('naics_code').isin([445110])).drop('naics_code')
   
   a = big_box_df.groupby('year','date').agg(collect_list('visits').alias('array_visits'))\
-                      .withColumn('median', F.ceil(F.size(F.col('array_visits'))/2)) #F.element_at(F.col('array_visits'), F.ceil((F.size(F.col('array_visits'))/2)).cast('int')))
+                      .withColumn('median', F.ceil(F.size(F.col('array_visits'))/2).cast('int')) #F.element_at(F.col('array_visits'), F.ceil((F.size(F.col('array_visits'))/2)).cast('int')))
   big_box_df.write.option("header",True).csv(f"{sys.argv[1]}/{a.dtypes}")
   #convenience_df.write.option("header",True).csv(f"{sys.argv[1]}/convenience_stores")
   #drinking_df.write.option("header",True).csv(f"{sys.argv[1]}/drinking_places")
