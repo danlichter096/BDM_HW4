24c896489bc7fc856f9b7ea185a9ea95f1b5ac79
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-05 15:31:57 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -56,7 +56,7 @@ def main(sc):
   specialty_df = joindf.where(F.col('naics_code').isin([445210,445220,445230,445291,445292,445299])).drop('naics_code')
   supermarkets_df = joindf.where(F.col('naics_code').isin([445110])).drop('naics_code')
   
-  a = big_box_df.groupby('year','date').agg(F.sort_array(F.collect_list('visits')).alias('array_visits'))\
+  a = big_box_df.groupby('year','date').agg(F.stddev_pop('visits').alias('std'), F.sort_array(F.collect_list('visits')).alias('array_visits'))\
               .withColumn('middle', F.ceil(F.size(F.col('array_visits'))/2).cast('int'))\
               .withColumn('median', F.element_at(F.col('array_visits'), 80)).drop('array_visits')
                       #.withColumn('median', F.element_at(F.col('array_visits'), F.col('middle')))\
