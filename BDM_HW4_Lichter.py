9831ea0ade2e8c26bc167db7376982acc4a0d398
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-05 15:45:48 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -59,8 +59,9 @@ def main(sc):
   a = big_box_df.groupby('year','date').agg(F.stddev_pop('visits').alias('std'), F.sort_array(F.collect_list('visits')).alias('array_visits'))\
               .withColumn('middle', F.ceil(F.size(F.col('array_visits'))/2).cast('int'))\
               .withColumn('std', F.round('std').cast('int'))\
-              .withColumn('median', F.col('array_visits')[F.col('middle')-F.lit(1)]).drop('array_visits')#F.element_at('array_visits', 80)).drop('array_visits')
-                      #.withColumn('median', F.element_at(F.col('array_visits'), F.col('middle')))\
+              .withColumn('median', F.col('array_visits')[F.col('middle')-F.lit(1)]).drop('array_visits')
+              .withColumn('low', F.when(F.col('median')-F.col('std')>0,F.col('median')-F.col('std')).otherwise(0)).drop('array_visits').drop('middle')
+  #      .withColumn('high', F.col('median')+F.col('std'))\
                        # .drop('array_visits') #F.element_at(F.col('array_visits'), F.ceil((F.size(F.col('array_visits'))/2)).cast('int')))
   a.write.option("header",True).csv(f"{sys.argv[1]}/a")
   #big_box_df.write.option("header", True).csv(f"{sys.argv[1]}/{a.dtypes}")
