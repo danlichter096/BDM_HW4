568eca3b5fbbfb09ccadb3d557f6bd269e5bf664
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-05 19:36:12 -0500

    Update BDM_HW4_Lichter.py

--- BDM_HW4_Lichter.py
+++ BDM_HW4_Lichter.py
@@ -48,13 +48,13 @@ def main(sc):
               .withColumn('high', F.col('median')+F.col('std')).drop('array_visits').drop('std')\
               .write.option("header", True).csv(f"{sys.argv[1]}/big_box_grocers")
 
-  '''joindf.where(F.col('naics_code').isin([445120]))\
-              .groupby('year','date').agg(F.stddev_pop('visits').cast('int').alias('std'), F.sort_array(F.collect_list('visits')).alias('array_visits'))\
-              .withColumn('median', F.col('array_visits')[F.ceil(F.size(F.col('array_visits'))/2).cast('int')-F.lit(1)])\
+  joindf.where(F.col('naics_code').isin([445120]))\
+              .groupby('year','date').agg(F.stddev_pop('visits').alias('std'), F.sort_array(F.collect_list('visits')).alias('array_visits'))\
+              .withColumn('median', F.col('array_visits')[F.ceil(F.size(F.col('array_visits'))/2)-F.lit(1)])\
               .withColumn('low', F.when(F.col('median')-F.col('std')>0,F.col('median')-F.col('std')).otherwise(0))\
               .withColumn('high', F.col('median')+F.col('std')).drop('array_visits').drop('std')\
               .write.option("header", True).csv(f"{sys.argv[1]}/convenience_stores")
-  
+  '''
   joindf.where(F.col('naics_code').isin([722410]))\
               .groupby('year','date').agg(F.stddev_pop('visits').cast('int').alias('std'), F.sort_array(F.collect_list('visits')).alias('array_visits'))\
               .withColumn('median', F.col('array_visits')[F.ceil(F.size(F.col('array_visits'))/2).cast('int')-F.lit(1)])\
